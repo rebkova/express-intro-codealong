ed0c3f20032594a46edf56d3afceccc6adf9bbea
Rebkova <rebkova@gmail.com>
2020-12-06 21:20:03 +0100

    codealong

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import data from "./data.json"
 
+
+console.log(`Data length: ${data.length}`)
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -16,6 +19,36 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+//callback function as a second argument
+app.get("/nominations", (req, res) => {
+  res.json(data)
+})
+
+app.get("/year/:nomYear", (req, res) => {
+  //define the parameter
+  const year = req.params.nomYear
+
+  //req.query = accessing queries that appear after the ? in the url
+  const showWon = req.query.won
+
+  //see if the query won is true or false
+  console.log(showWon)
+
+  //year is a string!
+  console.log({ year })
+
+  //filter based on the parameter // +year meaning turning string into a number
+  let nominationsFromYear = data.filter((item) => item.year_award === +year)
+
+  if (showWon) {
+    nominationsFromYear = nominationsFromYear.filter((item) => item.win)
+  }
+
+  //returns the filtered data
+  res.json(nominationsFromYear)
+
 })
 
 // Start the server
